4d0f656465fac9bdef5bc3e2df0918016ecf6b65
Islemcharni <163349543+Islemcharni@users.noreply.github.com>
2024-04-22 14:30:34 +0100

    Update main.c
    Something more useful

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
+
+ int main() {
+    
+    char *jours[] = {
+        "lundi",
+        "Mardi",
+        "mercredi",
+        "jeudi",
+        "vendredi",
+        "samedi",
+        "dimanche"
+    };
+    for (int i = 0; i < 7; i++) {
+        printf("Day %d: %s\n", i+1, jours[i]);
+    }
+
+    return 0;
 }
